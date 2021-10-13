2e449a24dfef7518ee4328df5047b36bc468be55
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-13 09:45:03 +0100

    banking application

--- src/test/java/africa/semicolon/bankingApplication/data/repositories/BankRepositoryImplTest.java
+++ src/test/java/africa/semicolon/bankingApplication/data/repositories/BankRepositoryImplTest.java
@@ -1,4 +1,71 @@
+package africa.semicolon.bankingApplication.data.repositories;
+
+import africa.semicolon.bankingApplication.data.models.Bank;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BankRepositoryImplTest {
-  
+    BankRepository bankRepository;
+    @BeforeEach
+    void setUp() {
+        bankRepository = new BankRepositoryImpl();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void save() {
+        Bank bank = new Bank();
+        bank.setId("32567892");
+        bank.setName("Bank Goodnews");
+        assertEquals(bank, bankRepository.save(bank));
+    }
+
+    @Test
+    void findByBankId() {
+        Bank bank = new Bank();
+        bank.setId("32567892");
+        bank.setName("Bank Goodnews");
+        Bank foundBank = bankRepository.save(bank);
+        assertEquals(foundBank, bankRepository.findByBankId("32567892"));
+    }
+
+    @Test
+    void delete() {
+        Bank bank = new Bank();
+        bank.setId("32567892");
+        bank.setName("Bank Goodnews");
+        bankRepository.save(bank);
+        assertNotNull(bankRepository.findByBankId("32567892"));
+        bankRepository.delete(bank);
+        assertNull(bankRepository.findByBankId("32567892"));
+    }
+
+    @Test
+    void testDelete() {
+        Bank bank = new Bank();
+        bank.setId("32567892");
+        bank.setName("Bank Goodnews");
+        bankRepository.save(bank);
+        assertNotNull(bankRepository.findByBankId("32567892"));
+        bankRepository.delete("32567892");
+        assertNull(bankRepository.findByBankId("32567892"));
+    }
+
+    @Test
+    void findAll() {
+        Bank bank = new Bank();
+        bank.setId("32567892");
+        bank.setName("Bank Goodnews");
+        bankRepository.save(bank);
+        bankRepository.save(bank);
+        bankRepository.save(bank);
+        bankRepository.save(bank);
+        assertEquals(4, bankRepository.findAll().size());
+    }
 }
--- src/test/java/africa/semicolon/bankingApplication/data/repositories/BvnRepositoryImplTest.java
+++ src/test/java/africa/semicolon/bankingApplication/data/repositories/BvnRepositoryImplTest.java
@@ -1,4 +1,71 @@
+package africa.semicolon.bankingApplication.data.repositories;
+
+import africa.semicolon.bankingApplication.data.models.Bvn;
+import africa.semicolon.bankingApplication.data.models.Customer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BvnRepositoryImplTest {
-  
+    BvnRepository bvnRepository;
+    @BeforeEach
+    void setUp() {
+
+        bvnRepository = new BvnRepositoryImpl();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void save() {
+        Customer customer = new Customer();
+        Bvn bvn = new Bvn("32456785", customer);
+        Bvn savedBvn = bvnRepository.save(bvn);
+        assertEquals(bvn, savedBvn);
+    }
+
+    @Test
+    void findByCustomerId() {
+        Customer customer = new Customer();
+        Bvn bvn = new Bvn("32456785", customer);
+        Bvn savedBvn = bvnRepository.save(bvn);
+        Bvn foundBvn = bvnRepository.findByCustomerId("32456785");
+        assertEquals(savedBvn,foundBvn);
+    }
+
+    @Test
+    void delete() {
+        Customer customer = new Customer();
+        Bvn bvn = new Bvn("32456785", customer);
+        Bvn savedBvn = bvnRepository.save(bvn);
+        assertNotNull(bvnRepository.findByCustomerId("32456785"));
+        bvnRepository.delete(bvn);
+        assertNull(bvnRepository.findByCustomerId("32456785"));
+    }
+
+    @Test
+    void testDelete() {
+        Customer customer = new Customer();
+        Bvn bvn = new Bvn("32456785", customer);
+        bvnRepository.save(bvn);
+        assertNotNull(bvnRepository.findByCustomerId("32456785"));
+        bvnRepository.delete("32456785");
+        assertNull(bvnRepository.findByCustomerId("32456785"));
+    }
+
+    @Test
+    void findAll() {
+        Customer customer = new Customer();
+        Bvn bvn = new Bvn("32456785", customer);
+        bvnRepository.save(bvn);
+        bvnRepository.save(bvn);
+        bvnRepository.save(bvn);
+        bvnRepository.save(bvn);
+        bvnRepository.save(bvn);
+        assertEquals(5, bvnRepository.findAll().size());
+    }
 }
