ba5b2ab6f03d732964b78f398d32c1c2ac5f29c5
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-13 09:48:36 +0100

    banking app

--- src/main/java/africa/semicolon/bankingApplication/data/repositories/BankRepositoryImpl.java
+++ src/main/java/africa/semicolon/bankingApplication/data/repositories/BankRepositoryImpl.java
@@ -1,2 +1,42 @@
-package africa.semicolon.bankingApplication.data.repositories;public class BankRepositoryImpl {
+package africa.semicolon.bankingApplication.data.repositories;
+
+import africa.semicolon.bankingApplication.data.models.Bank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankRepositoryImpl implements BankRepository{
+    private List<Bank> banks = new ArrayList<>();
+    @Override
+    public Bank save(Bank bank) {
+        banks.add(bank);
+        return findByBankId(bank.getId());
+    }
+
+    @Override
+    public Bank findByBankId(String id) {
+        for (Bank bank : banks){
+            if (bank.getId().equalsIgnoreCase(id)){
+                return bank;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void delete(Bank bank) {
+        banks.remove(bank);
+    }
+
+    @Override
+    public void delete(String id) {
+        Bank bank = new Bank();
+        delete(findByBankId(id));
+    }
+
+    @Override
+    public List<Bank> findAll() {
+        return banks;
+    }
 }
--- src/main/java/africa/semicolon/bankingApplication/data/repositories/BvnRepository.java
+++ src/main/java/africa/semicolon/bankingApplication/data/repositories/BvnRepository.java
@@ -1,2 +1,14 @@
-package africa.semicolon.bankingApplication.data.repositories;public interface BvnRepository {
+package africa.semicolon.bankingApplication.data.repositories;
+
+import africa.semicolon.bankingApplication.data.models.Bvn;
+
+import java.util.List;
+
+public interface BvnRepository {
+
+    Bvn save(Bvn bvn);
+    Bvn findByCustomerId(String id);
+    void delete(Bvn bvn);
+    void delete(String id);
+    List<Bvn> findAll();
 }
