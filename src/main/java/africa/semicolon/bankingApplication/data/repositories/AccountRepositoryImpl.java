a4223fd4265ad152edc0d8a9a0e121e250b22651
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-13 09:49:13 +0100

    banking app

--- src/main/java/africa/semicolon/bankingApplication/data/repositories/AccountRepositoryImpl.java
+++ src/main/java/africa/semicolon/bankingApplication/data/repositories/AccountRepositoryImpl.java
@@ -1,2 +1,43 @@
-package africa.semicolon.bankingApplication.data.repositories;public class AccountRepositoryImpl {
+package africa.semicolon.bankingApplication.data.repositories;
+
+import africa.semicolon.bankingApplication.data.models.Account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountRepositoryImpl implements AccountRepository{
+
+    private List<Account> accounts = new ArrayList<>();
+    @Override
+    public Account save(Account account) {
+        accounts.add(account);
+        return findByAccountid(account.getCustomerId());
+    }
+
+    @Override
+    public Account findByAccountid(String id) {
+
+        for (Account account : accounts){
+            if (account.getCustomerId().equalsIgnoreCase(id)){
+                return  account;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void delete(Account account) {
+            accounts.remove(account);
+    }
+
+    @Override
+    public void delete(String id) {
+        Account account = findByAccountid(id);
+        delete(account);
+    }
+
+    @Override
+    public List<Account> findAll() {
+        return accounts;
+    }
 }
