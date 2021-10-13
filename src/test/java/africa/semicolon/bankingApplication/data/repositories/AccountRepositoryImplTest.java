cae39666d92c26dc49304501d62e3d6365c3912d
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-13 09:47:23 +0100

    banking app

--- src/test/java/africa/semicolon/bankingApplication/data/repositories/AccountRepositoryImplTest.java
+++ src/test/java/africa/semicolon/bankingApplication/data/repositories/AccountRepositoryImplTest.java
@@ -1,4 +1,78 @@
+package africa.semicolon.bankingApplication.data.repositories;
+
+import africa.semicolon.bankingApplication.data.models.Account;
+import africa.semicolon.bankingApplication.data.models.AccountType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 class AccountRepositoryImplTest {
-  
+    AccountRepository accountRepository;
+    @BeforeEach
+    void setUp() {
+        accountRepository = new AccountRepositoryImpl();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void save() {
+        Account account = new Account();
+        account.setNumber("217300854");
+        account.setCustomerId("38878906");
+        account.setType(AccountType.SAVINGS);
+        Account savedAccount = accountRepository.save(account);
+        assertEquals(account, savedAccount);
+    }
+
+    @Test
+    void findByAccountid() {
+        Account account = new Account();
+        account.setNumber("217300854");
+        account.setCustomerId("38878906");
+        account.setType(AccountType.SAVINGS);
+        Account savedAccount = accountRepository.save(account);
+        Account foundAccount = accountRepository.findByAccountid("38878906");
+        assertEquals(savedAccount, foundAccount);
+    }
+
+    @Test
+    void delete() {
+        Account account = new Account();
+        account.setNumber("217300854");
+        account.setCustomerId("38878906");
+        account.setType(AccountType.SAVINGS);
+        accountRepository.save(account);
+        assertNotNull(accountRepository.findByAccountid("38878906"));
+        accountRepository.delete(account);
+        assertNull(accountRepository.findByAccountid("38878906"));
+    }
+
+    @Test
+    void testDelete() {
+        Account account = new Account();
+        account.setNumber("217300854");
+        account.setCustomerId("38878906");
+        account.setType(AccountType.SAVINGS);
+        accountRepository.save(account);
+        assertNotNull(accountRepository.findByAccountid("38878906"));
+        accountRepository.delete("38878906");
+        assertNull(accountRepository.findByAccountid("38878906"));
+    }
+
+    @Test
+    void findAll() {
+        Account account = new Account();
+        account.setNumber("217300854");
+        account.setCustomerId("38878906");
+        account.setType(AccountType.SAVINGS);
+        accountRepository.save(account);
+        accountRepository.save(account);
+        accountRepository.save(account);
+        accountRepository.save(account);
+        assertEquals(4, accountRepository.findAll().size());
+    }
 }
