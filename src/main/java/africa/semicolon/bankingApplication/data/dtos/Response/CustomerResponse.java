d34e62f25f04e6ea40c558713eca87ea4a4c3da1
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-16 18:25:20 +0100

    banking application

--- src/main/java/africa/semicolon/bankingApplication/data/dtos/Response/CustomerResponse.java
+++ src/main/java/africa/semicolon/bankingApplication/data/dtos/Response/CustomerResponse.java
@@ -1,2 +1,24 @@
-package africa.semicolon.bankingApplication.data.dtos.Response;public class CustomerResponse {
+package africa.semicolon.bankingApplication.data.dtos.Response;
+
+
+import africa.semicolon.bankingApplication.data.models.Customer;
+import lombok.Data;
+
+@Data
+public class CustomerResponse {
+
+    private String firstName;
+    private String lastName;
+    private String bvn;
+    private int numberOfAccounts;
+
+    public CustomerResponse (Customer customer){
+        firstName = customer.getFirstName();
+        lastName = customer.getLastName();
+        bvn = customer.getBvn();
+        numberOfAccounts = customer.getAccounts().size();
+
+
+    }
+
 }
