53b0d79c34c1f22988c9ab2027089318150af0dc
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-16 18:25:57 +0100

    banking application

--- src/main/java/africa/semicolon/bankingApplication/data/dtos/Request/CreateAccountRequest.java
+++ src/main/java/africa/semicolon/bankingApplication/data/dtos/Request/CreateAccountRequest.java
@@ -1,2 +1,14 @@
-package africa.semicolon.bankingApplication.data.dtos.Request;public class CreateAccountRequest {
+package africa.semicolon.bankingApplication.data.dtos.Request;
+
+import africa.semicolon.bankingApplication.data.models.AccountType;
+import lombok.Data;
+
+@Data
+public class CreateAccountRequest {
+
+    private String bankId;
+    private String firstName;
+    private AccountType accountType;
+    private String lastName;
+    private String bvn;
 }
