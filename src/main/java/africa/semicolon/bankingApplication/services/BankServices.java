102be4475da2df87589a82815d8c4d351c923d5d
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-16 18:24:04 +0100

    banking application

--- src/main/java/africa/semicolon/bankingApplication/services/BankServices.java
+++ src/main/java/africa/semicolon/bankingApplication/services/BankServices.java
@@ -1,2 +1,15 @@
-package africa.semicolon.bankingApplication.services;public interface BankServices {
+package africa.semicolon.bankingApplication.services;
+
+import africa.semicolon.bankingApplication.data.dtos.Request.CreateAccountRequest;
+import africa.semicolon.bankingApplication.data.models.Bank;
+
+import java.util.List;
+
+public interface BankServices {
+
+    public String createBank(String bankName);
+
+    public List<Bank> findAllBanks();
+
+    String createAccount(CreateAccountRequest createAccountRequest);
 }
